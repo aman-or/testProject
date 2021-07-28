cd910375dd15d21f0074db575fd45b4f6193b939
aman-or <a-o-b001@mail.ru>
2021-07-28 19:41:25 +0300

    add folder

---
+++ P1/taskmanager/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return render(request, '')
+
+
+def about(request):
+    return HttpResponse("<h4>About</h4>")
